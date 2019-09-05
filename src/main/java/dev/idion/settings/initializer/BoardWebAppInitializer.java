09ba401ea1b4045ac3e7ae9e250429f64dd4d02e
idionKim <ksundong@gmail.com>
2019-09-05 10:31:25 +0900

    Add WebAppInitializer implementation
    web.xml replacement

---
+++ src/main/java/dev/idion/settings/initializer/BoardWebAppInitializer.java
@@ -0,0 +1,31 @@
+package dev.idion.settings.initializer;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletRegistration;
+
+public class BoardWebAppInitializer implements WebApplicationInitializer {
+
+	private static final String CONFIG_LOCATION = "dev.idion.settings.config";
+	private static final String MAPPING_URL = "/*";
+
+	@Override
+	public void onStartup(javax.servlet.ServletContext servletContext) {
+		WebApplicationContext context = getContext();
+		servletContext.addListener(new ContextLoaderListener(context));
+		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping(MAPPING_URL);
+	}
+
+	private AnnotationConfigWebApplicationContext getContext() {
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		context.setConfigLocation(CONFIG_LOCATION);
+		return context;
+	}
+
+}
