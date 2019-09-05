827dbb56cdf4878c6ad05af09d3128a0a3ed60b9
idionKim <ksundong@gmail.com>
2019-09-05 14:13:28 +0900

    Add BoardControllerTest using mockito

---
+++ src/test/java/dev/idion/board/controller/BoardControllerTest.java
@@ -0,0 +1,39 @@
+package dev.idion.board.controller;
+
+import dev.idion.settings.config.RootConfig;
+import dev.idion.settings.config.ServletConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {RootConfig.class, ServletConfig.class})
+@WebAppConfiguration
+public class BoardControllerTest {
+	@InjectMocks
+	private BoardController boardController;
+
+	private MockMvc mockMvc;
+
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.initMocks(this);
+		mockMvc = MockMvcBuilders.standaloneSetup(boardController).build();
+	}
+
+	@Test
+	void testBoardController() throws Exception {
+		mockMvc.perform(get("/board")).andExpect(status().isOk());
+	}
+}
