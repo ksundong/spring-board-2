2e013eeae3ee09c95697af495a94ba002674e33d
idionKim <ksundong@gmail.com>
2019-09-05 11:10:15 +0900

    Add ServletConfig
    servlet-context.xml replacement

---
+++ src/main/java/dev/idion/settings/config/ServletConfig.java
@@ -0,0 +1,11 @@
+package dev.idion.settings.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "dev.idion.board" })
+public class ServletConfig {
+}
