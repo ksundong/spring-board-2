88910800a4098cd487d591f1906db3c6c2349265
IdionKim <ksundong@gmail.com>
2019-09-11 13:10:47 +0900

    Add Encoding Filter "UTF-8"

--- src/main/java/dev/idion/settings/config/WebConfig.java
+++ src/main/java/dev/idion/settings/config/WebConfig.java
@@ -1,6 +1,9 @@
 package dev.idion.settings.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
 
 public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -17,6 +20,12 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] {"/"};
+	}
+
+	@Override
+	protected  Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter("UTF-8");
+		return new Filter[] { characterEncodingFilter };
 	}
 
 }
